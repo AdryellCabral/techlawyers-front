783d584c8b4ecff3044ee299b88f20412e9b923f
Adryell Cabral <adryellluciano@gmail.com>
2022-03-18 09:35:29 -0300

    feat: home page created

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Button from "../../components/Button";
+import { Container, Content } from "./styles";
 
 const Home = () => {
-  return <h1>hellooooooo</h1>;
+  const history = useHistory();
+
+  const handleNavigation = (path) => {
+    return history.push(path);
+  };
+
+  return (
+    <Container>
+      <Content>
+        <h1>
+          Tech<span>.</span>lawyers
+        </h1>
+        <span>O maior data center de advogados do Brasil</span>
+        <div>
+          <Button onClick={() => handleNavigation("/signup")} whiteSchema>
+            Cadastre-se
+          </Button>
+          <Button onClick={() => handleNavigation("/login")}>Login</Button>
+        </div>
+      </Content>
+    </Container>
+  );
 };
 
 export default Home;
