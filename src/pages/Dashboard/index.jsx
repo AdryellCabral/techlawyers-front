51569425282ca97113b65e21b77b4d2bf5f3e5b6
Adryell Cabral <adryellluciano@gmail.com>
2022-03-18 12:01:59 -0300

    feat: dashboar page done

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+import { Container, CardsContainer, InputContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import { FiSearch } from "react-icons/fi";
+import Select from "../../components/Select";
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+import { useState } from "react";
+import api from "../../services/api";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const [list, setList] = useState([]);
+
+  const { register, handleSubmit } = useForm();
+
+  const loadAll = () => {
+    api
+      .get("/list_lawyers")
+      .then((response) => setList(response.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    loadAll();
+  }, []);
+
+  const loadAlphabetic = () => {
+    api
+      .get("/list_alphabetic")
+      .then((response) => setList(response.data))
+      .catch((err) => console.log(err));
+  };
+
+  const loadLastFive = () => {
+    api
+      .get("/list_last_5")
+      .then((response) => setList(response.data))
+      .catch((err) => console.log(err));
+  };
+
+  const onSubmitFunction = (data) => {
+    const { filter_option } = data;
+    filter_option === "all"
+      ? loadAll()
+      : filter_option === "alphabetic"
+      ? loadAlphabetic()
+      : loadLastFive();
+  };
+
+  const deleteButtonFunction = (id) => {
+    api.delete(`delete_lawyer/${id}`).then(() => {
+      loadAll();
+      toast.success("Registro deletado com sucesso!");
+    });
+  };
+
+  return (
+    <Container>
+      <InputContainer onSubmit={handleSubmit(onSubmitFunction)}>
+        <span>Filtrar resultados</span>
+        <section>
+          <Select
+            icon={FiSearch}
+            placeholder="Todos os cadastros"
+            register={register}
+            name="filter_option"
+            type="select"
+            error=""
+          >
+            <option value="all">Todos os cadastros</option>
+            <option value="alphabetic">Ordem aldabética</option>
+            <option value="last_five">5 mais recentes</option>
+          </Select>
+          <Button type="submit">Pesquisar</Button>
+        </section>
+      </InputContainer>
+      <CardsContainer>
+        {list.map((item) => (
+          <Card
+            name={item.name}
+            email={item.email}
+            phone_number={item.phone_number}
+            state={item.state}
+            oab_num={item.oab_number}
+            id={item._id}
+            key={item._id}
+            onClick={() => deleteButtonFunction(item._id)}
+          />
+        ))}
+      </CardsContainer>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,1 +1,42 @@
+import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 30px 0;
+`;
+
+export const InputContainer = styled.form`
+  flex: 1;
+  margin-top: 32px;
+  padding: 0 38px;
+  section {
+    display: flex;
+    > div {
+      max-width: 80%;
+      flex: 1;
+      margin-right: 16px;
+    }
+
+    button {
+      max-width: 260px;
+      height: 56px;
+      margin: 0;
+    }
+  }
+`;
+
+export const CardsContainer = styled.div`
+  padding: 38px 0;
+  margin: 32px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  /* div {
+    margin-top: 32px;
+    margin-right: 16px;
+  } */
+`;
